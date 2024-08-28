8a5033cd177345535226cb5203cd814109164a3a
manis <manish170898@gmail.com>
2024-08-28 20:51:17 +0530

    LinkedList Revised and Updated

--- src/main/java/org/dsa/LinkedList/L19ReverseNodesInKGroup.java
+++ src/main/java/org/dsa/LinkedList/L19ReverseNodesInKGroup.java
@@ -6,7 +6,7 @@ public class L19ReverseNodesInKGroup {
     public static void main(String[] args) {
         ListNode node = new ListNode(1,new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
         ListNode node1 = new ListNode(1,new ListNode(2));
-        reverseKGroup(node1, 2);
+        reverseKGroup(node, 2);
     }
 
     public static ListNode reverseKGroup(ListNode head, int k) {
---
+++ src/main/java/org/rough/Main.java
@@ -0,0 +1,6 @@
+package org.rough;
+
+public class Main {
+        public static void main(String[] args) {
+        }
+}
